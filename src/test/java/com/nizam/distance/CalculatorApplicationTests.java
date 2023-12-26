1db9f29a40a2c0906f1bc2503a8632c864ded525
nizamumkl <nizam.umkl@gmail.com>
2023-12-26 10:35:46 +0800

    Setting Up the Project: - Create a new Spring Boot project using Maven. - Set up the necessary dependencies for Spring Boot.

---
+++ src/test/java/com/nizam/distance/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nizam.distance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
